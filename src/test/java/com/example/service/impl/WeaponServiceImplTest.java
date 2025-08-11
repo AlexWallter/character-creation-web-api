46f155f83723f234762bccb4d85696959ba57db9
Alex Walter <alexwalter129@gmail.com>
2025-08-11 20:51:06 -0300

    add: test to the getAllWeapon method of weapon service layer

---
+++ src/test/java/com/example/service/impl/WeaponServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.example.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.character_creator.dto.WeaponDTO;
+import com.example.character_creator.enums.Dice;
+import com.example.character_creator.enums.WeaponQuality;
+import com.example.character_creator.model.Weapon;
+import com.example.character_creator.repository.WeaponRepo;
+import com.example.character_creator.service.impl.WeaponServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class WeaponServiceImplTest {
+    @Mock
+    private WeaponRepo repository;
+
+    @InjectMocks 
+    private WeaponServiceImpl weaponServiceImpl;
+
+    private Weapon weapon1;
+    private Weapon weapon2;
+
+    private WeaponDTO weaponDTO1 = new WeaponDTO(1L,
+                                                 "exemple", 
+                                                 "d6", 
+                                                 "sem qualidade", 
+                                                 "A arma não têm qualidades especificas.");
+
+    private WeaponDTO weaponDTO2 = new WeaponDTO(2L, 
+                                                "other exemple", 
+                                                "d8", 
+                                                "precisa", 
+                                                "Concede +1 em Testes de ataque.");
+    
+    @BeforeEach
+    void setup() {
+        weapon1 = Weapon.builder()
+        .id(1L)
+        .name("exemple")
+        .damage(Dice.D6)
+        .quality(WeaponQuality.NO_QUALITY)
+        .build();
+        
+        weapon1.setQualityDescription("sem qualidade");
+
+        weapon2 = Weapon.builder()
+        .id(2L)
+        .name("other exemple")
+        .damage(Dice.D8)
+        .quality(WeaponQuality.PRECISA)
+        .build();
+
+        weapon2.setQualityDescription("precisa");
+    }
+
+        @Test
+        void testGetAllWeapon() {
+        List<Weapon> allWeapons = List.of(weapon1, weapon2);
+        List<WeaponDTO> ExpectedWeaponDTOs = List.of(weaponDTO1, weaponDTO2);
+
+        when(repository.findAll()).thenReturn(allWeapons);
+
+        List<WeaponDTO> actualWeaponDTOs = weaponServiceImpl.getAllWeapons();
+
+        assertEquals(ExpectedWeaponDTOs, actualWeaponDTOs);
+
+        verify(repository).findAll();
+    }
+}
